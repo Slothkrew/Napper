6371527f56a625e303297643b33393c4642539b6
jsrn <james.srn@gmail.com>
2015-03-12 23:38:19 +0000

    dry

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,16 +7,17 @@ describe "The user class" do
   it "Should ensure unique username" do
     name = SecureRandom.hex
 
-    User.create({
-      username:        name,
-      password_digest: "derp"
-      })
+    create_user(name)
 
     expect {
-      User.create({
-        username:        name,
-        password_digest: "derp"
-        })
+      create_user(name)
     }.to change{ User.count }.by(0)
   end
+end
+
+def create_user(name)
+  User.create({
+    username:        name,
+    password_digest: "a_password_goes_here"
+    })
 end
